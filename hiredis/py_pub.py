a1e6f1ca083504d9ea44339eeca7f2fc97555b98
Wilbeibi <wilbeibi@gmail.com>
2017-03-30 12:15:59 -0700

    cleanup and clang-format

--- hiredis/py_pub.py
+++ hiredis/py_pub.py
@@ -1,23 +1,6 @@
 import redis
 ss =   """{
-   "ostd":{
-      "ost1":{
-         "type":"regular",
-         "lsu":"lsu"
-      },
-      "ost2":{
-         "type":"regular",
-         "lsu":"lsu"
-      },
-      "ost3":{
-         "type":"regular",
-         "lsu":"lsu",
-         "111":"111"
-      },
-      "serialnum":{
-         "serialnum":"00505693C854"
-      }
-   }
+    "Test": "pub"
 }"""
 
 if __name__ == "__main__":
