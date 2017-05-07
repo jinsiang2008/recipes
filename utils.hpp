b3b2a6ccbced4f5a0af4307985f8d04d37787dec
Wilbeibi <wilbeibi@gmail.com>
2017-05-06 18:56:25 -0700

    make_unique in C++11

---
+++ utils.hpp
@@ -0,0 +1,10 @@
+/** A collection of useful functions
+ */
+#include <memory>
+
+// Backport of C++14
+template<typename T, typename ...Args>
+std::unique_ptr<T> make_unique( Args&& ...args )
+{
+    return std::unique_ptr<T>( new T( std::forward<Args>(args)... ) );
+}
