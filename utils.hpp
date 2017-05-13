ae8857c12e3e14e9341255dbbc65295f3a727d4e
Wilbeibi <wilbeibi@gmail.com>
2017-05-13 00:59:10 -0700

    :cat:

--- utils.hpp
+++ utils.hpp
@@ -8,3 +8,10 @@ std::unique_ptr<T> make_unique( Args&& ...args )
 {
     return std::unique_ptr<T>( new T( std::forward<Args>(args)... ) );
 }
+
+// From rocksplicator
+using std::chrono::seconds;
+uint64_t GetCurrentTime() {
+  return std::chrono::duration_cast<seconds>(
+    std::chrono::system_clock::now().time_since_epoch()).count();
+}
