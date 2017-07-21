45b5dca1749eab8af2de4b02607eaa03638705fb
Wilbeibi <wilbeibi@gmail.com>
2017-07-21 16:36:42 -0700

    add python meta magic

---
+++ python/utils.py
@@ -0,0 +1,21 @@
+# Flatten embedded list
+from compiler.ast import flatten
+flatten([1, [2], [3, [[4]]])
+
+# Get docstring from a function
+def my_func():
+    "Docstring blabla"
+    pass
+my_func.__doc__
+
+# Get function signature
+# https://docs.python.org/3.4/library/inspect.html#inspect.signature
+
+# Call function by it's literal name
+# https://stackoverflow.com/questions/3061/calling-a-function-of-a-module-from-a-string-with-the-functions-name-in-python
+
+# Get list of methods in a class
+# https://stackoverflow.com/questions/1911281/how-do-i-get-list-of-methods-in-a-python-class
+# inspect.getmembers()
+
+
