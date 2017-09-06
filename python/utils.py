64260e51e9868a1fa658126256719cb5d692be82
Wilbeibi <wilbeibi@gmail.com>
2017-09-06 16:53:04 -0700

    add humanize_bytes

--- python/utils.py
+++ python/utils.py
@@ -21,3 +21,20 @@ my_func.__doc__
 # Get CPU numbers
 from multiprocessing import cpu_count
 print (cpu_count())
+
+# From github.com/dongweiming/web_develop.git
+def humanize_bytes(bytesize, precision=2):
+    abbrevs = (
+        (1 << 50, 'PB'),
+        (1 << 40, 'TB'),
+        (1 << 30, 'GB'),
+        (1 << 20, 'MB'),
+        (1 << 10, 'kB'),
+        (1, 'bytes')
+    )
+    if bytesize == 1:
+        return '1 byte'
+    for factor, suffix in abbrevs:
+        if bytesize >= factor:
+            break
+    return '%.*f %s' % (precision, bytesize / factor, suffix)
